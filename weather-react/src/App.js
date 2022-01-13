2c97270a98c98b52b60313ca03782cc85c8037b2
Moustadirane Taenchiki <moustadiranetaenchiki@10-21-201-173.dynapool.wireless.nyu.edu>
2022-01-13 16:34:54 -0500

    added date

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -3,9 +3,22 @@ import React from 'react';
 const api = {
     key: "511bed680f73bff65ec232c038a2b53a"
 }
-/* base: "https://api.openweathermap.org/data/2.5/" --> invalid */
+
 
 function App() {
+   const dateBuilder = (d) =>{
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()]; /* index num between 0-6, day of week*/
+    let date = d.getDate(); /*num between 0-31 */
+    let month = months[d.getMonth()] /* index num between 0-11, month of year*/
+    let year = d.getFullYear(); /* gets current year */
+
+    return`${day} ${month} ${date}, ${year}`
+   }
+
+
   return (
     <div className="app ">
         <main>
@@ -15,6 +28,12 @@ function App() {
                 className = "search-bar"
                 placeholder = "Search..."
                 />
+
+            </div>
+
+            <div className="location-box">
+                <div className="location">New York City, U.S.</div>
+                <div className="date">{dateBuilder(new Date())}</div>
 
             </div>
         </main>
