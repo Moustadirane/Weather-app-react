074f16eb20b7961c0af143c89b9703762644c83a
Moustadirane Taenchiki <moustadiranetaenchiki@10-21-201-173.dynapool.wireless.nyu.edu>
2022-01-13 15:19:16 -0500

    css

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -1,8 +1,23 @@
+import React from 'react';
+
+const api = {
+    key: "511bed680f73bff65ec232c038a2b53a"
+}
+/* base: "https://api.openweathermap.org/data/2.5/" --> invalid */
 
 function App() {
   return (
-    <div className="App">
+    <div className="app ">
+        <main>
+            <div className="search-box">
+                <input
+                type = "text"
+                className = "search-bar"
+                placeholder = "Search..."
+                />
 
+            </div>
+        </main>
     </div>
   );
 }
